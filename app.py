19a877f210e957536302a7a8f8b514d28dba42ca
Svitlana Vasylyeva <73995046+svasylyeva@users.noreply.github.com>
2021-03-18 15:44:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,41 +5,24 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
-# Dataset 'Processing'
+# Dataset Processing
 
-df_emissions = pd.read_csv('emission_full.csv')
+x = np.array([1, 2, 3, 4, 5])
+y = x * 2
 
-df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
+# Building our Graphs
 
-# Building our Graphs (nothing new here)
+data_scatter = dict(type='scatter',
+                    x=x,
+                    y=y,
+                    name='example'
+                    )
 
-data_choropleth = dict(type='choropleth',
-                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
-                       locationmode='country names',
-                       z=np.log(df_emission_0['CO2_emissions']),
-                       text=df_emission_0['country_name'],
-                       colorscale='inferno',
-                       colorbar=dict(title='CO2 Emissions log scaled')
+layout_scatter = dict(xaxis=dict(title='X axis'),
+                      yaxis=dict(title='Y axis')
                       )
 
-layout_choropleth = dict(geo=dict(scope='world',  #default
-                                  projection=dict(type='orthographic'
-                                                 ),
-                                  #showland=True,   # default = True
-                                  landcolor='black',
-                                  lakecolor='white',
-                                  showocean=True,   # default = False
-                                  oceancolor='azure'
-                                 ),
-                         
-                         title=dict(text='World Choropleth Map',
-                                    x=.5 # Title relative position according to the xaxis, range (0,1)
-                                   )
-                        )
-
-fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
-
-
+fig_scatter = go.Figure(data=data_scatter, layout=layout_scatter)
 
 # The App itself
 
@@ -47,24 +30,16 @@ app = dash.Dash(__name__)
 
 server = app.server
 
+app.layout = html.Div([
+    html.H1('My First DashBoard'),
 
-
-
-app.layout = html.Div(children=[
-    html.H1(children='My First DashBoard'),
-
-    html.Div(children='''
-        Example of html Container
-    '''),
+    html.Div('Example of html Container'),
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=fig_scatter
     )
 ])
-
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
