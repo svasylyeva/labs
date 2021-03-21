4dc9815e4869c87dbfaa81262c1460ef11389d6c
Svitlana Vasylyeva <73995046+svasylyeva@users.noreply.github.com>
2021-03-21 23:51:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,23 @@ x = np.array([1, 2, 3, 4, 5])
 y = x * 2
 
 # Building our Graphs
-cities = pd.read_csv('https://raw.githubusercontent.com/hflabs/city/master/city.csv')
-fig_map = go.Figure(go.Scattermapbox(lat=cities['geo_lat'], lon=cities['geo_lon']))
-fig_map.update_layout(mapbox_style="open-street-map")
+def to_int_year(value):
+    try:
+        return int(value)
+    except:
+        return None
+
+cities['foundation_year'] = cities['foundation_year'].map(to_int_year)
+cities = cities[['region', 'city', 'geo_lat', 'geo_lon', 'foundation_year', 'population']].dropna()
+
+fig_map = go.Figure(go.Scattermapbox(lat=cities['geo_lat'], 
+                                 lon=cities['geo_lon'], 
+                                 text=cities['city'],
+                                 marker=dict(colorbar=dict(title="Year of foundation"),
+                                             color=cities['foundation_year'],
+                                             size=cities['population'].map(to_int_size))))
+fig_map.update_layout(mapbox_style="open-street-map",
+                  mapbox=dict(center=map_center, zoom=2))
 
 
 # The App itself
@@ -23,9 +37,9 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.H1('Dashboard example'),
+    html.H1('Dashboard of Svitlana'),
 
-    html.Div('Example of html Container, MAP'),
+    html.Div('Example of map'),
 
     dcc.Graph(
         id='example-graph',
